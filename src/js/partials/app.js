2722659bf1c1a77db106432cdfd7e12156e906a5
AlexanderDolgan <5704737@gmai.com>
2016-07-26 20:47:32 +0300

    header

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -6,8 +6,24 @@ var main = function() {
         $('html,body').animate({scrollTop: tag.offset().top},'slow');
 
     });
+
+    $('.sec-menu-btn').click(function() {
+        $('.header-top-bar__nav').toggleClass('show');
+
+    });
 };
 
 $(document).ready(main);
 
 
+var slides = document.querySelectorAll('#slides .slides__item');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slides__item';
+    currentSlide = (currentSlide + 1)%slides.length;
+    slides[currentSlide].className = 'slides__item showing';
+}
+
+$(document).ready(nextSlide());
