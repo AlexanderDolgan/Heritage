e325004fd707723c260a8192cff560832e2355dd
Alexander Dolgan <5704737@gmail.com>
2016-07-28 10:00:46 +0300

    t

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,13 +1,13 @@
-var main = function() {
-    $('.terms__header').click(function() {
+var main = function () {
+    $('.terms__header').click(function () {
         $('.terms__content').toggleClass('hide');
 
         var tag = $('#terms__content');
-        $('html,body').animate({scrollTop: tag.offset().top},'slow');
+        $('html,body').animate({scrollTop: tag.offset().top}, 'slow');
 
     });
 
-    $('.sec-menu-btn').click(function() {
+    $('.sec-menu-btn').click(function () {
         $('.header-top-bar__nav').toggleClass('show');
 
     });
@@ -15,15 +15,14 @@ var main = function() {
 
 $(document).ready(main);
 
-
 var slides = document.querySelectorAll('#slides .slides__item');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,5000);
+var slideInterval = setInterval(nextSlide, 5000);
 
 function nextSlide() {
     slides[currentSlide].className = 'slides__item';
-    currentSlide = (currentSlide + 1)%slides.length;
+    currentSlide = (currentSlide + 1) % slides.length;
     slides[currentSlide].className = 'slides__item showing';
 }
 
-$(document).ready(nextSlide());
+$(document).ready(nextSlide);
